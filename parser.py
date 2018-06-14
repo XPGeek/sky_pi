982d29c2af8ebe185bb52075c43bc1e143496dda
XPGeek <samg5@vt.edu>
2018-06-14 10:39:14 -0400

    Working on parsing non infrastructure interfaces now.

--- parser.py
+++ parser.py
@@ -6,7 +6,6 @@ import re
 try:
 
     #Open File for reading:
-
     switch_config_file = open(sys.argv[1], 'r')
     switch_config = switch_config_file.read()
 
@@ -17,15 +16,15 @@ try:
     current_search_pattern = "show cdp nei | e SEP"
     index_a = switch_config.find(current_search_pattern)
 
-    if current_search_pattern == -1:
-        print("Error: Could not locate Infrastructure port Information")
+    if index_a == -1:
+        print("Error: Could not locate Infrastructure port Information, exiting.")
         exit()
 
     current_search_pattern = "Total cdp entries displayed :"
     index_b = switch_config[index_a:].find(current_search_pattern)
 
-    if current_search_pattern == -1:
-        print("Error: Could not locate Infrastructure port Information")
+    if index_b == -1:
+        print("Error: Could not locate Infrastructure port Information, exiting.")
         exit()
 
     #now that we have determined the area of interest, isolate
@@ -49,6 +48,7 @@ try:
         index_b = switch_config[index_a:].find("!")
         current_substring = switch_config[index_a:index_a+index_b]
 
+        #if the interface has something we don't like, we can flag it
         if current_substring.find("authentication") != -1:
             flagged_infrastructure_interfaces.append(("Authentication enabled on infrastructure interface " + infrastructure_interface, infrastructure_interface))
 
@@ -57,8 +57,48 @@ try:
 
         print(current_substring)
 
+    if len(infrastructure_ports) == 0 or len(infrastructure_interfaces) == 0:
+        print("Error: Could not locate any infrastructure ports/interfaces, exiting.")
+        exit()
 
+    #now, we can examine each non-infrastructure interface for compliance
+    current_search_pattern = "show run"
+    index_a = switch_config.find(current_search_pattern)
 
+    if index_a == -1:
+        print("Error: Could not locate run configuration, exiting.")
+        exit()
+
+    current_substring = switch_config[index_a:]
+
+    #find all interface names after a new line/return character
+    current_search_pattern = re.compile(r'[\n\r]([i][n][t][e][r][f][a][c][e][ ][^Vv].*)', re.MULTILINE)
+
+    #string list of all NON infrastructure interfaces
+    interfaces = list()
+
+    #PAIR list of all flagged interfaces, with STRING reason, STRING interface
+    flagged_interfaces = list()
+
+    for interface in re.finditer(current_search_pattern, current_substring):
+        if str(interface.group(1)) not in infrastructure_interfaces:
+            interfaces.append(str(interface.group(1)))
+            #isolate the configuration for each interface
+            index_a = current_substring.find(str(interface.group(1)))
+            index_b = current_substring[index_a:].find("!")
+
+            #contains the interface substring
+            interface_substring = current_substring[index_a:index_a+index_b]
+            interface_lines = interface_substring.count('\n')
+
+            #see if the interface is even configured
+            if interface_lines is 1:
+                flagged_interfaces.append(str(interface.group(1)) + " contains an empty configuration.", infrastructure_interface))
+                print("dongers")
+
+            print(interface_substring + "\n" + "Number of lines: " + str(interface_lines))
+
+    print("bing")
 
 
 finally:
